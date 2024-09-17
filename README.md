a49c7b404a01d398d96f866c2c22eccc5c9078cf
mmtaccos <aboyemj26@pwcs-edu.org>
2024-09-17 15:34:06 -0400

    added stuff

---
+++ README.md
@@ -0,0 +1,6 @@
+# **Welcome to The Offical 4303D 24-25 High Stakes GitHub repo!**
+This is where you will find the main comp file for robot's code! It includes:
+- Our drive controls
+- Comptetion autons
+- Skills auton
+- And much more!
---
+++ include/autons.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "EZ-Template/drive/drive.hpp"
+
+extern Drive chassis;
+
+// The function defs for the autons in autons.cpp
+// DO NOT REMOVE OR CODE WON'T WORK!!!!!
+
+// The function def for normal red auto
+void NormalRedAuto();
+// The function def for other red auto
+void OtherRedAuto();
+
+void default_constants();
